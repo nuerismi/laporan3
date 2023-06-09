39df9d2fc8ea9b461347abdc9843b47834f7ade4
DESKTOP-EFH71QH\Nur Ismi <ismijabbar@gmail.com>
2023-06-09 02:25:33 -0700

    Laporan 3

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,9 +20,26 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'template/css/core.css',
+        'template/css/components.css',
+        'template/css/icons.css',
+        'template/css/pages.css',
+        'template/css/menu.css',
+        'template/css/responsive',
     ];
     public $js = [
+        'template/js/modernizr.min.js',
+        'template/js/bootstrap.min.js',
+        'template/js/detect.js',
+        'template/js/fastclick.js',
+        'template/js/jquery.slimscroll.js',
+        'template/js/jquery.blockUI.js',
+        'template/js/waves.js',
+        'template/js/wow.min.js',
+        'template/js/jquery.nicescroll.js',
+        'template/js/jquery.scrollTo.min.js',
+        'template/js/jquery.core.js',
+        'template/js/jquery.app.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -9,7 +9,7 @@ use yii\bootstrap5\Breadcrumbs;
 use yii\bootstrap5\Html;
 use yii\bootstrap5\Nav;
 use yii\bootstrap5\NavBar;
-
+use yii\helpers\Url;
 AppAsset::register($this);
 
 $this->registerCsrfMetaTags();
@@ -28,56 +28,214 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 </head>
 <body class="d-flex flex-column h-100">
 <?php $this->beginBody() ?>
+<!-- Navigation Bar-->
+<header id="topnav">
+            <div class="topbar-main">
+                <div class="container">
 
-<header id="header">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top']
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'Mahasiswa', 'url' => ['/mahasiswa064/index']],
-            //['label' => 'About', 'url' => ['/site/about']],
-            //['label' => 'Contact', 'url' => ['/site/contact']],
-            //Yii::$app->user->isGuest
-            //    ? ['label' => 'Login', 'url' => ['/site/login']]
-            //    : '<li class="nav-item">'
-            //       . Html::beginForm(['/site/logout'])
-            //        . Html::submitButton(
-            //            'Logout (' . Yii::$app->user->identity->username . ')',
-            //            ['class' => 'nav-link btn btn-link logout']
-            //        )
-            //        . Html::endForm()
-            //        . '</li>'
-        ]
-    ]);
-    NavBar::end();
-    ?>
-</header>
+                    <!-- Logo container-->
+                    <div class="logo">
+                        <a href="index.html" class="logo"><span>Ub<i
+                                class="md md-album"></i>ld</span></a>
+                    </div>
+                    <!-- End Logo container-->
 
-<main id="main" class="flex-shrink-0" role="main">
-    <div class="container">
-        <?php if (!empty($this->params['breadcrumbs'])): ?>
-            <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
-        <?php endif ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</main>
 
-<footer id="footer" class="mt-auto py-3 bg-light">
-    <div class="container">
-        <div class="row text-muted">
-            <div class="col-md-6 text-center text-md-start">&copy; My Company <?= date('Y') ?></div>
-            <div class="col-md-6 text-center text-md-end"><?= Yii::powered() ?></div>
+                    <div class="menu-extras">
+
+                        <ul class="nav navbar-nav navbar-right pull-right">
+                            <li class="navbar-c-items">
+                                <form role="search" class="navbar-left app-search pull-left hidden-xs">
+                                     <input type="text" placeholder="Search..." class="form-control">
+                                     <a href=""><i class="fa fa-search"></i></a>
+                                </form>
+                            </li>
+                            <li class="dropdown navbar-c-items">
+                                <a href="#" data-target="#" class="dropdown-toggle waves-effect waves-light" data-toggle="dropdown" aria-expanded="true">
+                                    <i class="icon-bell"></i> <span class="badge badge-xs badge-danger">3</span>
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-lg">
+                                    <li class="notifi-title"><span class="label label-default pull-right">New 3</span>Notification</li>
+                                    <li class="list-group slimscroll-noti notification-list">
+                                       <!-- list item-->
+                                       <a href="javascript:void(0);" class="list-group-item">
+                                          <div class="media">
+                                             <div class="pull-left p-r-10">
+                                                <em class="fa fa-diamond noti-primary"></em>
+                                             </div>
+                                             <div class="media-body">
+                                                <h5 class="media-heading">A new order has been placed A new order has been placed</h5>
+                                                <p class="m-0">
+                                                    <small>There are new settings available</small>
+                                                </p>
+                                             </div>
+                                          </div>
+                                       </a>
+
+                                       <!-- list item-->
+                                       <a href="javascript:void(0);" class="list-group-item">
+                                          <div class="media">
+                                             <div class="pull-left p-r-10">
+                                                <em class="fa fa-cog noti-warning"></em>
+                                             </div>
+                                             <div class="media-body">
+                                                <h5 class="media-heading">New settings</h5>
+                                                <p class="m-0">
+                                                    <small>There are new settings available</small>
+                                                </p>
+                                             </div>
+                                          </div>
+                                       </a>
+
+                                       <!-- list item-->
+                                       <a href="javascript:void(0);" class="list-group-item">
+                                          <div class="media">
+                                             <div class="pull-left p-r-10">
+                                                <em class="fa fa-bell-o noti-custom"></em>
+                                             </div>
+                                             <div class="media-body">
+                                                <h5 class="media-heading">Updates</h5>
+                                                <p class="m-0">
+                                                    <small>There are <span class="text-primary font-600">2</span> new updates available</small>
+                                                </p>
+                                             </div>
+                                          </div>
+                                       </a>
+
+                                       <!-- list item-->
+                                       <a href="javascript:void(0);" class="list-group-item">
+                                          <div class="media">
+                                             <div class="pull-left p-r-10">
+                                                <em class="fa fa-user-plus noti-pink"></em>
+                                             </div>
+                                             <div class="media-body">
+                                                <h5 class="media-heading">New user registered</h5>
+                                                <p class="m-0">
+                                                    <small>You have 10 unread messages</small>
+                                                </p>
+                                             </div>
+                                          </div>
+                                       </a>
+
+                                        <!-- list item-->
+                                       <a href="javascript:void(0);" class="list-group-item">
+                                          <div class="media">
+                                             <div class="pull-left p-r-10">
+                                                <em class="fa fa-diamond noti-primary"></em>
+                                             </div>
+                                             <div class="media-body">
+                                                <h5 class="media-heading">A new order has been placed A new order has been placed</h5>
+                                                <p class="m-0">
+                                                    <small>There are new settings available</small>
+                                                </p>
+                                             </div>
+                                          </div>
+                                       </a>
+
+                                       <!-- list item-->
+                                       <a href="javascript:void(0);" class="list-group-item">
+                                          <div class="media">
+                                             <div class="pull-left p-r-10">
+                                                <em class="fa fa-cog noti-warning"></em>
+                                             </div>
+                                             <div class="media-body">
+                                                <h5 class="media-heading">New settings</h5>
+                                                <p class="m-0">
+                                                    <small>There are new settings available</small>
+                                                </p>
+                                             </div>
+                                          </div>
+                                       </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0);" class="list-group-item text-right">
+                                            <small class="font-600">See all notifications</small>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li class="dropdown navbar-c-items">
+                                <a href="" class="dropdown-toggle waves-effect waves-light profile" data-toggle="dropdown" aria-expanded="true"><img src="assets/images/users/avatar-1.jpg" alt="user-img" class="img-circle"> </a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="javascript:void(0)"><i class="ti-user text-custom m-r-10"></i> Profile</a></li>
+                                    <li><a href="javascript:void(0)"><i class="ti-settings text-custom m-r-10"></i> Settings</a></li>
+                                    <li><a href="javascript:void(0)"><i class="ti-lock text-custom m-r-10"></i> Lock screen</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="javascript:void(0)"><i class="ti-power-off text-danger m-r-10"></i> Logout</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <div class="menu-item">
+                            <!-- Mobile menu toggle-->
+                            <a class="navbar-toggle">
+                                <div class="lines">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                            </a>
+                            <!-- End mobile menu toggle-->
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="navbar-custom">
+                <div class="container">
+                    <div id="navigation">
+                        <!-- Navigation Menu-->
+                        <ul class="navigation-menu">
+                            <li class="has-submenu">
+                                <a href="a"><i class="md md-dashboard"></i>Master Data</a>
+                                <ul class="submenu">
+                                    <li>
+                                        <a href="<?= Url::to(['mahasiswa064/index'])?>"> mahasiswa </a>
+                                    </li>
+                                </ul>
+                        </ul>
+                        <!-- End navigation menu        -->
+                    </div>
+                </div> <!-- end container -->
+            </div> <!-- end navbar-custom -->
+        </header>
+        <!-- End Navigation Bar-->
+
+
+        <div class="wrapper">
+            <div class="container">
+
+              <?= $content ?>
+
+                <!-- Footer -->
+                <footer class="footer text-right">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xs-6">
+                                © 2016. All rights reserved.
+                            </div>
+                            <div class="col-xs-6">
+                                <ul class="pull-right list-inline m-b-0">
+                                    <li>
+                                        <a href="#">About</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Help</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+                <!-- End Footer -->
+
+            </div> <!-- end container -->
         </div>
-    </div>
-</footer>
-
+        <!-- end wrapper -->
 <?php $this->endBody() ?>
 </body>
 </html>
